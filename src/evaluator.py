728225c3816d798177dbd7a6317031ec518779e7
Bhrij B Patel <bhrijp11@gmail.com>
2025-12-27 21:42:21 -0500

    Update evaluator.py

--- src/evaluator.py
+++ src/evaluator.py
@@ -1,15 +1,14 @@
 from abc import ABC, abstractmethod
 from typing import Dict, Any
 
-class Evaluator(ABC):
-    """
-    Abstract base class for an Evaluator.
-    """
-    
-    @abstractmethod
-    def evaluate(self, prompt: str, code: str) -> Dict[str, Any]:
+class Evaluator:
+
+    def __init__(self, model: str = "gpt-4o-mini"):
+        self.model = model
+   
+    def evaluate(self, sys_prompt: str, user_prompt: str) -> Dict[str, Any]:
         """
-        Evaluates the generated code against the prompt.
+        Evaluates the generated code against the task prompt.
         
         Args:
             prompt: The original problem description.
@@ -18,4 +17,6 @@ class Evaluator(ABC):
         Returns:
             A dictionary containing evaluation metrics (e.g., {"score": 1.0, "is_correct": True}).
         """
-        pass
+      return get_llm_response(sys_prompt=sys_prompt, 
+                        user_prompt=user_prompt, 
+                        model=self.model)
